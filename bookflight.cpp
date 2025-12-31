074b3b93fc92a53cdb3bb372979a1985c9041e22
lishacodesgames <lishastudies.cs@gmail.com>
2026-01-01 01:31:46 +0530

    Added event loop

--- bookflight.cpp
+++ bookflight.cpp
@@ -28,6 +28,25 @@ int main() {
   cout<<"\033c";
   
   Airport flight;
+  int choice;
+  cout << "******************* LISHA'S AMAZING AIRPORT *******************\n";
+  while(true) {
+    cout << "\n0. Exit";
+    cout << "\n1. Book a flight";
+    cout << "\n2. Show flight details";
+    cout << "\nWhat would you like to do? ";
+    cin >> choice;
+
+    switch(choice) {
+      case 0: exit(0); 
+
+      case 1: break;
+      
+      case 2: break;
+
+      default: cout << "Invalid choice.\n"; break;
+    }
+  }
   
   cout<<'\n';
   return 0;
