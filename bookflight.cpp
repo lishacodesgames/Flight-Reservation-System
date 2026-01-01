ccb2eeda60dcdb61a75e74b7a8ecbe58619046af
lishacodesgames <lishastudies.cs@gmail.com>
2026-01-01 15:58:33 +0530

    Cleaned up book(), fixed infinite loops, initialised parseFlights() which I'll get back to later

--- bookflight.cpp
+++ bookflight.cpp
@@ -41,45 +41,8 @@ public:
     generateFlight(); 
   }
   void generateFlight();
-
-  void book(string how) {
-    if(how == "City") {
-      printTitle();
-      do {
-        cout << "Which city are you departing from? ";
-        cin >> City1;
-      } while(validCity(Cities, City1));
-
-      printTitle();
-      cout << "City of Departure: " << City1 << "\n\n";
-
-      ifstream flights("flights.txt");
-      cin.ignore();
-      
-      while(getline(flights, ID, '|')) {
-        flights.seekg(1, ios::cur); // moves cursor ahead by 1 char
-
-        string tempCity1;
-        getline(flights, tempCity1, '-'); flights.seekg(1, ios::cur);
-
-        getline(flights, City2, '|'); flights.seekg(1, ios::cur);
-
-        string tempSeats;
-        getline(flights, tempSeats, '/');
-        takenSeats = stoi(tempSeats);
-        getline(flights, tempSeats);
-        totalSeats = stoi(tempSeats);
-
-        if(tempCity1 == City1) {
-          cout << "Flight " << ID << "\n";
-          cout << City1 << " -> " << City2 << "\n";
-          cout << "Seats available: " << totalSeats-takenSeats << "/" << totalSeats;
-        }
-      }
-
-      flights.close();
-    }
-  }
+  void book(string how);
+  void parseFlight();
 };
 
 int main() {
@@ -110,16 +73,16 @@ int main() {
           cout << "\n2. Choose from Flight Number";
           cout << "\nHow would you like to book? ";
           cin >> choice;
-        }
-
-        switch(choice) {
-          case 0: break;
-
-          case 1: flight.book("City"); break;
-
-          case 2: break;
-
-          default: cout << "\nInvalid choice";
+          
+          switch(choice) {
+            case 0: validChoice = false; printTitle(); break;
+            
+            case 1: flight.book("City"); break;
+            
+            case 2: break;
+            
+            default: cout << "\nInvalid choice"; break;
+          }
         }
         break;
       }
@@ -156,4 +119,59 @@ void Flights::generateFlight() {
   flights << ID << "||" << City1 << "->" << City2 << "||" << takenSeats << "/" << totalSeats<<endl;
 
   flights.close();
+}
+
+void Flights::book(string how) {
+  if(how == "City") {
+    printTitle();
+    while(true) {
+      cout << "Which city are you departing from? ";
+      cin >> City1;
+
+      if(validCity(Cities, City1)) break;
+      else {printTitle(); cout << "Invalid City.\n";}
+    }
+
+    printTitle();
+    cout << "City of Departure: " << City1;
+
+    ifstream flights("flights.txt");
+    cin.ignore();
+    
+    while(getline(flights, ID, '|')) {
+      flights.seekg(1, ios::cur); // moves cursor ahead by 1 char
+      
+      string tempCity1;
+      string tempSeats;
+
+      getline(flights, tempCity1, '-'); flights.seekg(1, ios::cur);
+
+      getline(flights, City2, '|'); flights.seekg(1, ios::cur);
+
+      getline(flights, tempSeats, '/');
+      takenSeats = stoi(tempSeats);
+      getline(flights, tempSeats);
+      totalSeats = stoi(tempSeats);
+
+      if(tempCity1 == City1) {
+        cout << "\n\nFlight " << ID << "\n";
+        cout << City1 << " -> " << City2 << "\n";
+        cout << "Seats available: " << totalSeats-takenSeats << "/" << totalSeats;
+      }
+    }
+
+    flights.close();
+  }
+
+  else {
+    ifstream flights("flights.txt");
+
+    
+
+    flights.close();
+  }
+}
+
+void Flights::parseFlight() {
+  
 }
