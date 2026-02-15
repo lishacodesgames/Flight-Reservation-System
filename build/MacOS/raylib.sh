3773dced618a3e73dd064965740840e84e78286b
Lisha Patil <lishastudies.cs@gmail.com>
2026-02-15 20:57:24 +0530

    Added texture, not loading

--- build/MacOS/raylib.sh
+++ build/MacOS/raylib.sh
@@ -1,1 +1,1 @@
-clang++ "src/raylib/buttons.cpp" -Iinclude/ -L lib/ -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL lib/libraylib.a -o build/Mac/RaylibButtons && ./build/Mac/RaylibButtons
+clang++ "src/raylib/buttons.cpp" -Iinclude/ -L lib/ -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL lib/libraylib.a -o binaries/RaylibButtons && ./binaries/RaylibButtons
--- src/raylib/buttons.cpp
+++ src/raylib/buttons.cpp
@@ -4,13 +4,17 @@ int main() {
   InitWindow(600, 600, "Buttons");
   SetTargetFPS(60);
 
+  Texture2D background = LoadTexture("../graphics/bg.jpg");
+
   while(!WindowShouldClose()) {
     BeginDrawing();
 
-    ClearBackground(PINK);
+    ClearBackground(BLACK);
+    DrawTexture(background, 10, 10, WHITE);
     
     EndDrawing();
   }
 
+  UnloadTexture(background);
   CloseWindow();
 }
